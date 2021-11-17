34b29dcb7a0b019b428babb9afcfe13c8855308f
Dmitrii Gordienko <dimentr12170@gmail.com>
2021-11-17 20:05:02 +0500

    refactor: score calculation service tests refactoring

--- BowlingScore/Tests/ScoreCalculationServiceTests.cs
+++ BowlingScore/Tests/ScoreCalculationServiceTests.cs
@@ -2,21 +2,24 @@ using NUnit.Framework;
 
 namespace BowlingScore.Tests
 {
+    [TestFixture]
     public class ScoreCalculationTests
     {
-        private Score _score = new Score();
+        private Score _score;
         private ScoreCalculationService _scoreCalculationService;
         
         [SetUp]
         public void Setup()
         {
+            _score = new Score();
             _scoreCalculationService = new ScoreCalculationService(_score);
         }
 
         [Test]
         public void Calculation_One_Throw_Returns_One_Frame()
         {
-            _scoreCalculationService.CalculateThrow("9");
+            var throws = new [] {"9"};
+            CalculationManyThrows(throws);
 
             Assert.AreEqual(1, _score.Frames.Count);
         }
@@ -24,10 +27,18 @@ namespace BowlingScore.Tests
         [Test]
         public void Calculation_Two_Throw_Returns_One_Frame()
         {
-            _scoreCalculationService.CalculateThrow("9");
-            _scoreCalculationService.CalculateThrow("2");
+            var throws = new [] {"9", "2"};
+            CalculationManyThrows(throws);
 
             Assert.AreEqual(1, _score.Frames.Count);
+        }
+
+        private void CalculationManyThrows(string[] throws)
+        {
+            foreach (var thr in throws)
+            {
+                _scoreCalculationService.CalculateThrow(thr);
+            }
         }
     }
 }
