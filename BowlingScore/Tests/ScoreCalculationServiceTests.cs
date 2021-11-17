40fa2e0ea78273321ce34d157d4f45faaa2857d9
Dmitrii Gordienko <dimentr12170@gmail.com>
2021-11-17 19:30:31 +0500

    refactor: tests refactoring

--- BowlingScore/Tests/ScoreCalculationServiceTests.cs
+++ BowlingScore/Tests/ScoreCalculationServiceTests.cs
@@ -4,30 +4,30 @@ namespace BowlingScore.Tests
 {
     public class ScoreCalculationTests
     {
+        private Score _score = new Score();
+        private ScoreCalculationService _scoreCalculationService;
+        
         [SetUp]
         public void Setup()
         {
+            _scoreCalculationService = new ScoreCalculationService(_score);
         }
 
         [Test]
         public void Calculation_One_Throw_Returns_One_Frame()
         {
-            var scoreCalculationService = new ScoreCalculationService();
+            _scoreCalculationService.CalculateThrow("9");
 
-            scoreCalculationService.CalculateThrow("9");
-
-            Assert.AreEqual(1, scoreCalculationService.Score.Frames.Count);
+            Assert.AreEqual(1, _score.Frames.Count);
         }
         
         [Test]
         public void Calculation_Two_Throw_Returns_One_Frame()
         {
-            var scoreCalculationService = new ScoreCalculationService();
+            _scoreCalculationService.CalculateThrow("9");
+            _scoreCalculationService.CalculateThrow("2");
 
-            scoreCalculationService.CalculateThrow("9");
-            scoreCalculationService.CalculateThrow("2");
-
-            Assert.AreEqual(1, scoreCalculationService.Score.Frames.Count);
+            Assert.AreEqual(1, _score.Frames.Count);
         }
     }
 }
